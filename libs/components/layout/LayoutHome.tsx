cf67d404be161bf3034b834cb1351cd8721e43fa
Sharipov Abdulatif <138142557+Abdulatif90@users.noreply.github.com>
2026-06-25 13:21:33 +0900

    Refactor LayoutHome component by cleaning imports
    Removed unused imports and lifecycle method for JWT token handling.
    
    Signed-off-by: Sharipov Abdulatif <138142557+Abdulatif90@users.noreply.github.com>

--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, ComponentType } from 'react';
+import React, { ComponentType } from 'react';
 import useDeviceDetect from '../../hooks/useDeviceDetect';
+import useAuthSync from '../../hooks/useAuthSync';
 import Head from 'next/head';
 import Top from '../Top';
 import Footer from '../Footer';
 import { Stack } from '@mui/material';
 import HeaderFilter from '../homepage/HeaderFilter';
-import { getJwtToken, updateUserInfo } from '../../auth';
 import Chat from '../Chat';
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -14,13 +14,9 @@ import 'swiper/css/navigation';
 const withLayoutMain = <P extends object>(Component: ComponentType<P>) =>{
 	const WrappedComponent = (props: any) => {
 		const device = useDeviceDetect();
+		useAuthSync();
 
-		/** LIFECYCLES **/
-		useEffect(() => {
-			const jwt = getJwtToken();
-			if (jwt) updateUserInfo(jwt);
-		}, []);
-
+	
 		/** HANDLERS **/
 
 	ruturn (
