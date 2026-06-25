fafccde3bb57688435af1f234c14d4bc5e2528c8
Sharipov Abdulatif <138142557+Abdulatif90@users.noreply.github.com>
2026-06-25 12:13:12 +0900

    Refactor withLayoutBasic to use PAGE_META
    Signed-off-by: Sharipov Abdulatif <138142557+Abdulatif90@users.noreply.github.com>

--- libs/components/layout/LayoutBasic.tsx
+++ libs/components/layout/LayoutBasic.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, ComponentType  } from 'react';
 import { useRouter } from 'next/router';
 import useDeviceDetect from '../../hooks/useDeviceDetect';
 import Head from 'next/head';
@@ -7,85 +7,80 @@ import Footer from '../Footer';
 import { Stack } from '@mui/material';
 import { getJwtToken, updateUserInfo } from '../../auth';
 import Chat from '../Chat';
-import { useReactiveVar } from '@apollo/client';
-import { userVar } from '../../../apollo/store';
 import { useTranslation } from 'next-i18next';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-const withLayoutBasic = (Component: any) => {
-	return (props: any) => {
-		const router = useRouter();
-		const { t, i18n } = useTranslation('common');
-		const device = useDeviceDetect();
-		const [authHeader, setAuthHeader] = useState<boolean>(false);
-		const user = useReactiveVar(userVar);
+const PAGE_META: Record<string, { title: string; desc: string; bgImage: string }> = {
+    '/property': {
+        title: 'property.search.title',
+        desc: 'property.search.desc',
+        bgImage: '/img/banner/2.jpg',
+    },
+    '/agent': {
+        title: 'agent.title',
+        desc: 'agent.desc',
+        bgImage: '/img/banner/agents.webp',
+    },
+    '/agent/detail': {
+        title: 'agent.detail.title',
+        desc: 'agent.detail.desc',
+        bgImage: '/img/banner/header2.svg',
+    },
+    '/mypage': {
+        title: 'mypage.title',
+        desc: 'mypage.desc',
+        bgImage: '/img/banner/header1.svg',
+    },
+    '/community': {
+        title: 'community.title',
+        desc: 'community.desc',
+        bgImage: '/img/banner/header2.svg',
+    },
+    '/community/detail': {
+        title: 'community.detail.title',
+        desc: 'community.detail.desc',
+        bgImage: '/img/banner/header2.svg',
+    },
+    '/cs': {
+        title: 'cs.title',
+        desc: 'cs.desc',
+        bgImage: '/img/banner/header2.svg',
+    },
+    '/account/join': {
+        title: 'auth.title',
+        desc: 'auth.desc',
+        bgImage: '/img/banner/header2.svg',
+    },
+    '/member': {
+        title: 'member.title',
+        desc: 'member.desc',
+        bgImage: '/img/banner/header1.svg',
+    },
+    '/about': {
+        title: 'about.title',
+        desc: 'about.desc',
+        bgImage: '/img/banner/2.jpg',
+    },
+};
+const DEFAULT_META = {
+    title: 'default.title',
+    desc: 'default.desc',
+    bgImage: '/img/banner/default.jpg',
+};
 
-		const memoizedValues = useMemo(() => {
-			let title = '',
-				desc = '',
-				bgImage = '';
-
-			switch (router.pathname) {
-				case '/property':
-					title = 'Property Search';
-					desc = 'We are glad to see you again!';
-					bgImage = '/img/banner/2.jpg';
-					break;
-				case '/agent':
-					title = 'Agents';
-					desc = 'Home / For Rent';
-					bgImage = '/img/banner/agents.webp';
-					break;
-				case '/agent/detail':
-					title = 'Agent Page';
-					desc = 'Home / For Rent';
-					bgImage = '/img/banner/header2.svg';
-					break;
-				case '/mypage':
-					title = 'my page';
-					desc = 'Home / For Rent';
-					bgImage = '/img/banner/header1.svg';
-					break;
-				case '/community':
-					title = 'Community';
-					desc = 'Home / For Rent';
-					bgImage = '/img/banner/header2.svg';
-					break;
-				case '/community/detail':
-					title = 'Community Detail';
-					desc = 'Home / For Rent';
-					bgImage = '/img/banner/header2.svg';
-					break;
-				case '/cs':
-					title = 'CS';
-					desc = 'We are glad to see you again!';
-					bgImage = '/img/banner/header2.svg';
-					break;
-				case '/account/join':
-					title = 'Login/Signup';
-					desc = 'Authentication Process';
-					bgImage = '/img/banner/header2.svg';
-					setAuthHeader(true);
-					break;
-				case '/member':
-					title = 'Member Page';
-					desc = 'Home / For Rent';
-					bgImage = '/img/banner/header1.svg';
-					break;
-				case '/about':
-					title = 'ABOUT';
-					desc = 'Thank you for contacting us!';
-					bgImage = '/img/banner/2.jpg';
-					break;
-				default:
-					break;
-			}
-
-			return { title, desc, bgImage };
-		}, [router.pathname]);
-
+const withLayoutBasic = <P extends object>(Component: ComponentType<P>) => {
+    const WrappedComponent = (props: P) => {
+        const router = useRouter();
+        const { t } = useTranslation('common');
+        const device = useDeviceDetect();
+	    const isAuthHeader = router.pathname === '/account/join';
+        const meta = PAGE_META[router.pathname] ?? DEFAULT_META;
+        const title = meta.title;
+        const desc = meta.desc;
+		const bgImage = meta.bgImage;
+	
 		/** LIFECYCLES **/
 		useEffect(() => {
 			const jwt = getJwtToken();
@@ -93,15 +88,17 @@ const withLayoutBasic = (Component: any) => {
 		}, []);
 
 		/** HANDLERS **/
+	return(
+	<>
+		<Head>
+		  <title>{title ? `${t(title)} | Aurux` : 'Aurux'}</title>
+		  <meta name="description" content={t(desc)} />
+		  <meta property="og:title" content={t(title)} />
+		  <meta property="og:description" content={t(desc)} />
+		</Head>
 
-		if (device == 'mobile') {
-			return (
-				<>
-					<Head>
-						<title>Aurux</title>
-						<meta name={'title'} content={`Aurux`} />
-					</Head>
-					<Stack id="mobile-wrap">
+		{device ==='mobile'? (
+				   <Stack id="mobile-wrap">
 						<Stack id={'top'}>
 							<Top />
 						</Stack>
@@ -114,48 +111,39 @@ const withLayoutBasic = (Component: any) => {
 							<Footer />
 						</Stack>
 					</Stack>
-				</>
-			);
-		} else {
-			return (
-				<>
-					<Head>
-						<title>Aurux</title>
-						<meta name={'title'} content={`Aurux`} />
-					</Head>
+			)
+		 :
+			(
 					<Stack id="pc-wrap">
 						<Stack id={'top'}>
 							<Top />
 						</Stack>
-
 						<Stack
-							className={`header-basic ${authHeader && 'auth'}`}
+							className={`header-basic ${isAuthHeader ? 'auth' : ''}`}
 							style={{
-								backgroundImage: `url(${memoizedValues.bgImage})`,
+								backgroundImage: `url(${bgImage})`,
 								backgroundSize: 'cover',
 								boxShadow: 'inset 10px 40px 150px 40px rgb(24 22 36)',
 							}}
 						>
 							<Stack className={'container'}>
-								<strong>{t(memoizedValues.title)}</strong>
-								<span>{t(memoizedValues.desc)}</span>
+								<strong>{t(title)}</strong>
+								<span>{t(desc)}</span>
 							</Stack>
 						</Stack>
-
 						<Stack id={'main'}>
 							<Component {...props} />
 						</Stack>
-
 						<Chat />
-
 						<Stack id={'footer'}>
 							<Footer />
 						</Stack>
 					</Stack>
-				</>
-			);
-		}
-	};
+			)}
+	</>
+	)};
+	WrappedComponent.displayName = `withLayoutBasic(${Component.displayName || Component.name || 'Component'})`;
+    return WrappedComponent;      
 };
 
 export default withLayoutBasic;
